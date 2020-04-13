8b5a8b5b9ef655cb29b374783a66be6e13bdf441
summaiya14 <summaiyamahmod@gmail.com>
2020-04-12 22:34:07 -0700

    header

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => {
+
+	return (
+		<Header headerText='Albums'/>
+		
+		
+	);
+};
+
+
+
+AppRegistry.registerComponent('albums', () => App);
